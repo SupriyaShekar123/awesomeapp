c8f669fe7ec6dfd538c63bf1596413716737dbc6
SupriyaShekar123 <s.supriya82@gmail.com>
2020-04-27 18:30:39 +0200

    pokemon data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon"
 
 function App() {
   return (
     <div className="App">
     <main>
-      <Title />
+      <Title content="Some Simple Text"/>
+      <Pokemon />
     </main>
 
 <header className="App-header">
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 
- export default function Title() {
-    return <h1> Hi there! </h1>
+ export default function Title(props) {
+    return <h1> {props.content} </h1>
 }
