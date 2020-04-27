85bd4ea2594a084833145a89358aa2a24aeadd55
SupriyaShekar123 <s.supriya82@gmail.com>
2020-04-27 20:14:23 +0200

    pokemon data parsed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,76 @@ import './App.css';
 import Title from "./components/Title";
 import Pokemon from "./components/Pokemon"
 
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"]
+  }
+];
+
+const  nameOf = all_pokemon.map((namePokemon)=>{
+  return namePokemon.name
+})
+const  weightOf = all_pokemon.map((weightPokemon)=>{
+  return weightPokemon.weight
+})
+
+const awesomeOf = all_pokemon.map((awsomePokemon)=>{
+  return awsomePokemon.awesome
+})
+const terrifyingOf = all_pokemon.map((terrifyingPokemon)=>{
+  return terrifyingPokemon.weight
+})
+const abilitiesOf = all_pokemon.map((abilitiesPokemon)=>{
+  return (<li>{abilitiesPokemon.abilities}</li>)
+})
+
+
+
 function App() {
   return (
     <div className="App">
     <main>
-      <Title content="Some Simple Text"/>
-      <Pokemon />
+      <Title content="Some Simple Text" />
+
+      
+
+      
+      <Pokemon name={nameOf[0]}
+               weight = {weightOf[0]}
+               awesome = {awesomeOf[0]}
+               terrifying ={terrifyingOf[0]}
+               abilities ={abilitiesOf[0]}
+      
+               
+      />
+      
+      
     </main>
 
 <header className="App-header">
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-export default function Pokemon(){
+export default function Pokemon(props){
     return(
         <div>
-            <h2>Pokemon name: {"Charizard"}</h2>
-      <p>Weight: {90} kg</p>
-      <p>Awesome: {true}</p>
-      <p>Terrifying: {false}</p>
-      <p>What about null? {null}</p>
-      <p>And undefined? {undefined}</p>
-      <p>Abilities: {["Blaze", "Solar power", "Tough claws", "Drought"]}</p>
+            <h2>Pokemon name: {props.name}</h2>
+      <p>Weight: {props.weight} kg</p>
+      <p>Awesome: { props.awesome ? "Very" : "nope,not really"}</p>
+      <p>Terrifying: {props.terrifying ? "Very" : "nah, loveable"}</p>
+      <p>Abilities: {props.abilities}</p>
+      
         </div>
     )
 }
